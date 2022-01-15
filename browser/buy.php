6199c42f42d8505254fb631952eaee312b789cae
HaykZiroyan <haik.ziroyan@mail.ru>
2022-01-15 22:55:10 +0400

    pdo

---
+++ browser/buy.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>online shop</title>
+    <link rel="icon" href="/images/download.png">
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+    <div id = "card" class="card-container">
+        <?php
+            include '../classes.php';
+            // $db = new DBase;
+            $mysql = new mysqli('127.0.0.1', 'root', 'root', 'new-tasks');
+
+            $user = 'root';
+            $password = 'root';
+            $host = '127.0.0.1';
+            $dbname = 'new-tasks';
+            $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+
+            // $pdo = getDb();
+        
+            $result = $pdo->query("SELECT * FROM `products`");
+            // $rows = $result->fetch_assoc();
+            $array = array();
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                array_push($array, ["id"=>$row["id"], "price"=> $row["price"], "description"=> $row["description"], "name"=> $row["name"]]); 
+            }
+            $array1 = array();
+            $result->execute();
+            $row_count =$result->fetchColumn();
+            for ($i = 0; $i < count($array)/5; $i++) {
+                array_push($array1, array());
+                for ($j = 0; $j < 5; $j++) {
+                    $k = 5*$i + $j;
+                    array_push($array1[$i], $array[$k]);
+                    
+
+                    if ($k+1 == count($array)) {
+                            break;
+                        }
+                }
+            }
+            for ($i = 0; $i < count($array1); $i++) {
+          
+                    ?>
+                
+                    <div class="cont">
+
+                    <?php
+                    
+                    $curr_cont_cards  =   $array1[$i];
+                    foreach ($curr_cont_cards as $card){
+                    ?>
+                            <form class="card" action="newfunc.php" method="post">
+                                <h3><?=$card["name"];?></h3>
+                                <p><?=$card["description"];?></p>
+                                <h2 class="price"><?=$card["price"];?></h2>
+                                <input type="hidden" name="id" value="<?= $card[id] ?>">
+                                <input type = "number" value='1' name="count">
+                                <button class="set-busket" type="submit">add to busket</button>
+                            </form>
+                    <?php } ?>
+                        </div> 
+               <?php  
+            }
+
+            
+    
+        ?>
+    </div>
+    <a href="karzina.php" class="karzina scrollKar">
+        <img src="/images/basket.png" alt="">
+        <div id="New" class="num">
+            <p>0</p>
+        </div>
+    </a>
+    
+    
+    
+</body>
+</html>
+<?php
+    // $pdo->close();
+?>
---
+++ for admin/orders.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/browser/styles/main.css">
+</head>
+<body>
+    <table>
+        <tr>
+            <th>order id</th>
+            <th>order date</th>
+            <th>firstname</th>
+            <th>lastname</th>
+            <th>email</th>
+            <th>product name</th>
+            <th>description</th>
+            <th>price</th>
+            <th>Quantity</th>
+            <th>total price</th>
+        </tr>
+        <?php
+            // $mysql = new mysqli('127.0.0.1', 'root', 'root', 'new-tasks');
+            // $orders = $mysql->query("SELECT * FROM `gagik`"); // ordera get date
+            $user = 'root';
+            $password = 'root';
+            $host = '127.0.0.1';
+            $dbname = 'new-tasks';
+            $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+
+            $pdo = new PDO($dsn, $user, $password);
+            $orders = $pdo->query("SELECT * FROM `gagik`");
+
+            while ($row = $orders->fetch(PDO::FETCH_ASSOC)) {
+                $userId = $row[user_id];
+                $users =  $pdo->query("SELECT * FROM `gago` WHERE `id` = $userId"); //users info
+                $userInfo = $users->fetch(PDO::FETCH_ASSOC);
+                $productIn =  $pdo->query("SELECT * FROM `gagul` WHERE `order_id` = $row[id]"); //products included in order
+                $prod = $productIn->fetch(PDO::FETCH_ASSOC);
+                $productId = $prod[product_id];
+                $info =  $pdo->query("SELECT * FROM `products` WHERE `id` = $productId"); //product info
+                $productInfo = $info->fetch(PDO::FETCH_ASSOC);
+                
+                ?>
+                <tr>
+                    <th><?php echo($row[id]) ?></th>
+                    <th><?php echo($row[order_date]) ?></th>
+                    <th><?php echo($userInfo[first_name]) ?></th>
+                    <th><?php echo($userInfo[last_name]) ?></th>
+                    <th><?php echo($userInfo[email]) ?></th>
+                    <th><?php echo($productInfo[name]) ?></th>
+                    <th><?php echo($productInfo[description]) ?></th>
+                    <th><?php echo($productInfo[price]) ?></th>
+                    <th><?php echo($prod[qty]) ?></th>
+                    <th><?php echo($productInfo[price] * $prod[qty]) ?></th>
+                </tr>
+
+                <?php
+                while ($row1 = $productIn->fetch(PDO::FETCH_ASSOC)) {
+                    $userId1 = $row1[user_id];
+                    $users1 =  $pdo->query("SELECT * FROM `gago` WHERE `id` = $userId1"); //users info
+                    // $userInfo1 = mysqli_fetch_assoc($users1);
+                    $productIn1 =  $pdo->query("SELECT * FROM `gagul` WHERE `order_id` = $row1[id]"); //products included in order
+                    $prod1 = $productIn1->fetch(PDO::FETCH_ASSOC);
+                    $productId1 = $row1[product_id];
+                    $info1 =  $pdo->query("SELECT * FROM `products` WHERE `id` = $productId1"); //product info
+                    $productInfo1 = $info1->fetch(PDO::FETCH_ASSOC);
+                    ?>
+                    
+
+                    <tr>
+                        <th><?php echo($userId1); ?> </th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th><?php echo($productInfo1[name]) ?></th>
+                        <th><?php echo($productInfo1[description]) ?></th>
+                        <th><?php echo($productInfo1[price]) ?></th>
+                        <th><?php echo($row1[qty]) ?></th>
+                        <th><?php echo($productInfo1[price] * $row1[qty]) ?></th>
+                    </tr>
+                    <?php
+                }
+            }
+
+        ?>
+    </table>
+</body>
+</html>
+
+
+
